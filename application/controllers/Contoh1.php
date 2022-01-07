89f95e49794ab484655e470a127e9ff5b9599ed7
Dev-Jaka <Jakafitriansyah31@gmail.com>
2022-01-07 16:55:45 +0700

    Upload Tugas Web Programming II - 1 s/d 4

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Jaka Fitriansyah, <br> Saya tinggal di daerah Cikarang olahraga yang saya sukai adalah Basket";
+    }
+}
---
+++ application/views/view-form-matakuliah.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Programming II</title>
+</head>
+
+<body>
+    <center>
+        <form action="<?= base_url('matakuliah/cetak') ?>" method="POST">
+
+            <table>
+                <tr>
+                    <th colspan="3"> Form Input Data Mata Kuliah </th>
+                </tr>
+                <tr>
+                    <td colspan="3">
+                        <hr>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Kode MTK</th>
+                    <td>:</td>
+                    <td> <input type="text" name="kode" id="kode"> </td>
+                    <td><span style="color: red; font-size:10px;"><?= form_error('kode') ?></span></td>
+                </tr>
+                <tr>
+                    <th>Nama MTK</th>
+                    <td>:</td>
+                    <td> <input type="text" name="nama" id="nama"> </td>
+                    <td><span style="color: red; font-size:10px;"><?= form_error('nama') ?></span></td>
+                </tr>
+                <tr>
+                    <th>SKS</th>
+                    <td>:</td>
+                    <td> <select name="sks" id="sks">
+                            <option value="">Pilih SKS</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                        </select> </td>
+                </tr>
+                <tr>
+                    <td colspan="3" align="center"> <input type="submit" value="Submit"> </td>
+                </tr>
+            </table>
+        </form>
+    </center>
+</body>
+
+</html>
